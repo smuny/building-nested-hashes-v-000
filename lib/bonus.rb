f2a967bc072af393795b2391dcce7dfd26c53cf7
Sharon Mun <sharonymun@gmail.com>
2018-07-02 19:42:54 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,7 @@ def bonus
    }
   }
 
-  #code your solution here:
-
+  capulet[:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
